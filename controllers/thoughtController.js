89c965bed52d1f3339e885966e758946cec3e9b7
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-25 17:32:36 -0800

    ✨ feat: add updateThought method in thoughtController 🛠️
    - Implemented functionality to update a thought by its ID ✏️
    - Validates input data and applies updates to the database ✅
    - Returns a 404 error if the thought is not found ❌
    - Handles server errors with appropriate responses 🚨

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -37,3 +37,17 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+   // Update a thought by ID
+   updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought found with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
