b085d8362e3b7d381b749ffbcc43d1cb0b7f333c
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-25 17:11:37 -0800

    feat: implement thought retrieval methods in thoughtController
    - Added `getThoughts` method to fetch all thoughts from the database
    - Added `getSingleThought` method to fetch a specific thought by ID
      - Returns a 404 error if the thought is not found
      - Handles server errors with appropriate status codes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,22 @@
+// thoughtController.js
+
+const { Thought, User } = require('../models');
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Get a single thought by ID
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought found with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
