7cd137c588f67237aa12e350e0d0e9759c433bf2
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-25 16:12:50 -0800

    feat: implement createUser and deleteUser controller methods
    - Added `createUser` method to create a new user in the database
      - Responds with the created user or an error
    - Added `deleteUser` method to delete a user by ID
      - Deletes associated thoughts after removing the user
      - Returns appropriate responses for success or missing user

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,3 +7,18 @@ module.exports = {
       .then(users => res.json(users))
       .catch(err => res.status(500).json(err));
   },
+  createUser(req, res) {
+    User.create(req.body)
+      .then(user => res.json(user))
+      .catch(err => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then(user => {
+        if (!user) return res.status(404).json({ message: 'No user found' });
+        return Thought.deleteMany({ _id: { $in: user.thoughts } });
+      })
+      .then(() => res.json({ message: 'User and thoughts deleted' }))
+      .catch(err => res.status(500).json(err));
+  },
+};
