7b2837ef3b786ce7f7ec6c6fee868b87dc99746e
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-25 17:58:37 -0800

    ✨ feat: implement addReaction method in thoughtController 💬
    - Added functionality to add a reaction to a thought by its ID 🧠
    - Ensured data validation with `runValidators` during update ✅
    - Returns a 404 error if the thought is not found ❌
    - Handles server errors with appropriate responses 🚨
    - Provides the updated thought with the added reaction on success 🎉

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -70,4 +70,17 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+   // Add a reaction to a thought
+   addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $push: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought found with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
